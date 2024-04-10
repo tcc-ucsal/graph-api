ad0b1a302ae88fac1c249e8f9d82e65617b5d89e
Ana Paula Tartari <anatartari412@gmail.com>
2024-04-10 19:32:02 -0300

    Initial project

---
+++ src/test/java/br/graphpedia/graphapi/GraphApiApplicationTests.java
@@ -0,0 +1,13 @@
+package br.graphpedia.graphapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GraphApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
