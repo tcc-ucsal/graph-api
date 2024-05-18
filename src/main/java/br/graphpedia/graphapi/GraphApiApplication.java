f53a1c99ad081f3fb8a97d1d00d0ecf352151f7a
Ana Paula Tartari <anatartari412@gmail.com>
2024-05-18 20:04:40 -0300

    Add entities

--- src/main/java/br/graphpedia/graphapi/GraphApiApplication.java
+++ src/main/java/br/graphpedia/graphapi/GraphApiApplication.java
@@ -2,8 +2,11 @@ package br.graphpedia.graphapi;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.data.neo4j.repository.config.EnableNeo4jRepositories;
 
-@SpringBootApplication
+@EnableNeo4jRepositories
+@SpringBootApplication(exclude = {DataSourceAutoConfiguration.class })
 public class GraphApiApplication {
 
 	public static void main(String[] args) {
