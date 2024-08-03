05939304c78003f699d1c2de3c37110de07da3e5
Ana Paula Tartari <anatartari412@gmail.com>
2024-08-03 17:02:55 -0300

    add elastic config

--- src/main/java/br/graphpedia/graphapi/infra/config/ElasticsearchConfig.java
+++ src/main/java/br/graphpedia/graphapi/infra/config/ElasticsearchConfig.java
@@ -2,27 +2,32 @@ package br.graphpedia.graphapi.infra.config;
 
 import br.graphpedia.graphapi.infra.database.elasticsearch.repository.ElasticsearchContextTermRepository;
 import org.springframework.beans.factory.annotation.Value;
-import org.springframework.data.elasticsearch.client.ClientConfiguration;
-import org.springframework.data.elasticsearch.client.elc.ElasticsearchConfiguration;
+import org.springframework.context.annotation.Bean;
+import org.springframework.data.elasticsearch.client.elc.ReactiveElasticsearchConfiguration;
 import org.springframework.data.elasticsearch.repository.config.EnableElasticsearchRepositories;
+import org.springframework.data.elasticsearch.client.ClientConfiguration;
 
 @org.springframework.context.annotation.Configuration
 @EnableElasticsearchRepositories(basePackageClasses = {ElasticsearchContextTermRepository.class})
-public class ElasticsearchConfig extends ElasticsearchConfiguration {
+public class ElasticsearchConfig {
 
     @Value("${spring.elasticsearch.uris}")
     private String elasticsearchUri;
 
     @Value("${spring.elasticsearch.username}")
-    private String username;
+    private String elasticsearchUsername;
 
     @Value("${spring.elasticsearch.password}")
-    private String password;
-    @Override
+    private String elasticsearchPassword;
+
+
+    @Bean
     public ClientConfiguration clientConfiguration() {
         return ClientConfiguration.builder()
                 .connectedTo(elasticsearchUri)
+                .withBasicAuth(elasticsearchUsername, elasticsearchPassword)
                 .build();
 
     }
+
 }
