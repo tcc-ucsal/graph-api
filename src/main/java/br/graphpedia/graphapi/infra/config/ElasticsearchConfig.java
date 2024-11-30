6e758dd2f75361e6a87156f260a9e89218bbe86d
Ana Paula Tartari <anatartari412@gmail.com>
2024-11-30 15:57:30 -0300

    add setup elastic index

--- src/main/java/br/graphpedia/graphapi/infra/config/ElasticsearchConfig.java
+++ src/main/java/br/graphpedia/graphapi/infra/config/ElasticsearchConfig.java
@@ -1,8 +1,13 @@
 package br.graphpedia.graphapi.infra.config;
 
+import br.graphpedia.graphapi.infra.database.elasticsearch.entity.TermContextEntity;
 import br.graphpedia.graphapi.infra.database.elasticsearch.repository.ElasticsearchContextTermRepository;
+import jakarta.annotation.PostConstruct;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
+import org.springframework.data.elasticsearch.core.ElasticsearchOperations;
+import org.springframework.data.elasticsearch.core.mapping.IndexCoordinates;
 import org.springframework.data.elasticsearch.repository.config.EnableElasticsearchRepositories;
 import org.springframework.data.elasticsearch.client.ClientConfiguration;
 
@@ -20,6 +25,12 @@ public class ElasticsearchConfig {
     @Value("${spring.elasticsearch.password}")
     private String elasticsearchPassword;
 
+    private final ElasticsearchOperations elasticsearchOperations;
+
+    @Autowired
+    public ElasticsearchConfig(ElasticsearchOperations elasticsearchOperations) {
+        this.elasticsearchOperations = elasticsearchOperations;
+    }
 
     @Bean
     public ClientConfiguration clientConfiguration() {
@@ -27,6 +38,32 @@ public class ElasticsearchConfig {
                 .connectedTo(elasticsearchUri)
                 .withBasicAuth(elasticsearchUsername, elasticsearchPassword)
                 .build();
+
+
+    }
+
+    @PostConstruct
+    public void verifyAndCreateIndexOnStartup() {
+        String indexName = "term_context";
+        Class<?> entityClass = TermContextEntity.class;
+
+        try{
+            IndexCoordinates indexCoordinates = IndexCoordinates.of(indexName);
+            boolean indexExists = elasticsearchOperations.indexOps(indexCoordinates).exists();
+
+            if (!indexExists) {
+                boolean created = elasticsearchOperations.indexOps(indexCoordinates).create();
+                if (created) {
+                    elasticsearchOperations.indexOps(indexCoordinates).putMapping(
+                            elasticsearchOperations.indexOps(entityClass).createMapping()
+                    );
+                } else {
+                    throw new RuntimeException("Elastic Search index error: " + indexName);
+                }
+            }
+        }catch (Exception e){
+            throw new RuntimeException(e);
+        }
 
 
     }
